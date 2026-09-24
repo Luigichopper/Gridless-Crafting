8da24600b73d21dc677b998f0ae2d87f361ce281
Luigichopper <luigichopper01@gmail.com>
2026-09-23 19:49:31 -0500

    Update RecipeManagerMixin descriptor for 1.21.4 RecipeMap

--- common/src/main/java/com/gridless/mixin/RecipeManagerMixin.java
+++ common/src/main/java/com/gridless/mixin/RecipeManagerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeMap;
 import net.minecraft.world.item.crafting.RecipeManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +14,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Map;
-
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
 
@@ -22,8 +21,8 @@ public class RecipeManagerMixin {
     @Final
     private HolderLookup.Provider registries;
 
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("TAIL"))
-    private void onApply(Map<?, ?> map, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
+    @Inject(method = "apply(Lnet/minecraft/world/item/crafting/RecipeMap;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("TAIL"))
+    private void onApply(RecipeMap map, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
         RecipeManager self = (RecipeManager) (Object) this;
         RecipeIndexer.reindex(self, this.registries);
     }
